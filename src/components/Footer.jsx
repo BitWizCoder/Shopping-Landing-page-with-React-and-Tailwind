cce417da33f0ae53c85b1f218b4bebb1b04506af
مُحَمَّد <mhmdnoman>
2023-08-04 12:06:22 +0600

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { IconContext } from "react-icons";
 import { FaFacebookF } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa6";
@@ -7,18 +8,24 @@ import { FaLinkedinIn } from "react-icons/fa6";
 function Footer() {
   return (
     <footer className="bg-slate-900">
-      <div className="container mx-auto text-white py-12 flex justify-between">
-        <div className="flex flex-col gap-3">
+      <div className="container mx-auto text-center lg:text-left text-white py-12 flex flex-col lg:flex-row lg:justify-between gap-5">
+        <div className="flex flex-col gap-3 items-center lg:items-start">
           <h1 className="text-4xl font-bold">FASHION</h1>
-          <p className="text-[#8E8E8E]">Complete your style with awesome <br /> clothes from us.</p>
+          <p className="text-[#8E8E8E]">
+            Complete your style with awesome <br /> clothes from us.
+          </p>
           <div className="flex">
-            <FaFacebookF />
-            <FaInstagram />
-            <FaTwitter />
-            <FaLinkedinIn />
+            <IconContext.Provider
+              value={{ backgroundColor: "blue", className: "global-class-name" }}
+            >
+              <FaFacebookF size={18} color="gray" background-color="red" />
+              <FaInstagram />
+              <FaTwitter />
+              <FaLinkedinIn />
+            </IconContext.Provider>
           </div>
         </div>
-        <div className="flex gap-24">
+        <div className="flex gap-5 lg:gap-24 flex-col lg:flex-row">
           <div>
             <h2 className="text-2xl">Company</h2>
             <div className="flex flex-col text-[#8E8E8E]">
